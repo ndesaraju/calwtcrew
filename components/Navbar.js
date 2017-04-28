e9ae8ed30a770170349ea4d3bb24be95ff3392cb
Cindy Tung <cindytung@berkeley.edu>
2017-04-28 16:35:01 -0700

    responsive navigation bar (#8)
    * added started component files
    
    * changed component names and author
    
    * buggy
    
    * working on about me design
    
    * basic layout done
    
    * added real images
    
    * is responsive now
    
    * removed a comment
    
    * (attempt) to fix spacing issues in scss files
    
    * (attempt2) to fix spaces
    
    * SPACING SHOULD BE FIXED NOW
    
    * Update aboutme.scss
    
    * case sensitive file name fixes
    
    * case senstive file name changes recognized
    
    * made some navbar tweaks, did the alumni page
    
    * thanks alex for the donate now buttoNgit status graphic design is my passion
    
    * fixed css name
    
    * buggy hamburger navbar
    
    * finished ham menu
    
    * finished ham menu
    
    * removed react-button from dependencies

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,43 +1,97 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router';
-
-import '../css/navbar.scss';
+import Headroom from 'react-headroom';
+import classNames from 'classnames';
 import logo from '../assets/images/logo.png';
 
-const Navbar = () => (
-  <div className="navbar">
-    <div className="logo">
-      <Link to="/" className="link">
-        <img src={logo} alt="logo" className="logo__image" />
-      </Link>
-    </div>
-    <div className="links">
-      <Link to="/about/" className="link">
-        ABOUT US
-      </Link>
-      <div className="dropdown">
-        <button className="dropbtn">ROSTERS</button>
-        <div className="dropdown__content">
-          <Link to="/mens-roster/" className="link">
-            <div className="dropdown__link">MEN&#39;S ROSTER</div>
-          </Link>
-          <br />
-          <Link to="/womens-roster/" className="link">
-            <div className="dropdown__link">WOMEN&#39;S ROSTER</div>
-          </Link>
-        </div>
-      </div>
-      <Link to="/schedule/" className="link">
-        SCHEDULE
-      </Link>
-      <Link to="/alumni/" className="link">
-        ALUMNI
-      </Link>
-      <Link to="/join/" className="link">
-        JOIN US
-      </Link>
-    </div>
-  </div>
-);
+export default class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+      maxHeight: 50,
+    };
 
-export default Navbar;
+    this._handleNavOpen = this._handleNavOpen.bind(this);
+  }
+
+  _handleNavOpen(e) {
+    e.preventDefault();
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+    return (
+      <div>
+        <Headroom
+          wrapperStyle={{
+            maxHeight: this.state.maxHeight,
+          }}
+          style={{
+            background: 'rgba(255, 255, 255, 0.99)',
+          }}
+        >
+          <div
+            className={classNames('navbar', {
+              'navbar--open': this.state.open,
+            })}
+          >
+            <div className="logo">
+              <Link to="/" className="link">
+                <img src={logo} alt="logo" className="logo__image" />
+              </Link>
+            </div>
+            <div
+              className={classNames('links', 'navbar__hamburger', {
+                'navbar__hamburger--active': this.state.open,
+              })}
+              onClick={this._handleNavOpen}
+            >
+              <div className="hamburger__bar bar" />
+              <div className="hamburger__bar bar" />
+              <div className="hamburger__bar bar" />
+            </div>
+            <div className="links">
+              <Link to="/about/" className="link">
+                ABOUT US
+              </Link>
+              <div className="roster">
+                ROSTER
+                <Link to="/roster/men/" className="link__navbar--open">
+                  MEN&#39;S
+                </Link>
+                <Link to="/roster/men/" className="link__navbar--open">
+                  WOMEN&#39;S
+                </Link>
+              </div>
+              <div className="dropdown">
+                <button className="dropbtn">ROSTERS</button>
+                <div className="dropdown__content">
+                  <Link to="/roster/men/" className="link">
+                    <div className="dropdown__link">MEN&#39;S ROSTER</div>
+                  </Link>
+                  <br />
+                  <Link to="/roster/women/" className="link">
+                    <div className="dropdown__link">WOMEN&#39;S ROSTER</div>
+                  </Link>
+                </div>
+              </div>
+              <Link to="/schedule/" className="link">
+                SCHEDULE
+              </Link>
+              <Link to="/alumni/" className="link">
+                ALUMNI
+              </Link>
+              <Link to="/join/" className="link">
+                JOIN US
+              </Link>
+            </div>
+          </div>
+        </Headroom>
+      </div>
+    );
+  }
+}
