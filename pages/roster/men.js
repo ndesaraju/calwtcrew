c1fdfce401dfdf573ecc9a4a00fc1b826bca176c
Cindy Tung <cindytung@berkeley.edu>
2017-04-29 22:02:32 -0700

    Fix roster pages (#12)
    * added started component files
    
    * changed component names and author
    
    * buggy
    
    * working on about me design
    
    * basic layout done
    
    * added real images
    
    * is responsive now
    
    * removed a comment
    
    * (attempt) to fix spacing issues in scss files
    
    * (attempt2) to fix spaces
    
    * SPACING SHOULD BE FIXED NOW
    
    * Update aboutme.scss
    
    * case sensitive file name fixes
    
    * case senstive file name changes recognized
    
    * made some navbar tweaks, did the alumni page
    
    * thanks alex for the donate now buttoNgit status graphic design is my passion
    
    * fixed css name
    
    * fixed roster pages

--- pages/roster/men.js
+++ pages/roster/men.js
@@ -2,272 +2,380 @@ import React from 'react';
 import Roster from '../../components/Roster';
 import photo from '../../assets/images/mensroster.jpg';
 
-import '../../css/rosters.scss';
+import '../../css/Roster.scss';
 
-const RosterPage = () => (
-  <div className="container">
-    <div className="page__header">
-      2015-2016 Varsity & Novice Men
+const RosterPage = () => {
+  const varsityPlayers = [
+    {
+      name: 'Amos Frank',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'Vancouver, BC',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Andy Chung',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Burbank, CA',
+      major: 'Economics',
+    },
+    {
+      name: 'Arthur Sebes',
+      side: 'No Preference',
+      year: 'Grad Student',
+      hometown: 'Lyon, France',
+      major: 'Transportation Engineering',
+    },
+    {
+      name: 'Ben Bavonese',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Ann Arbor, MI',
+      major: 'Economics',
+    },
+    {
+      name: 'Brian Osgood',
+      side: 'Port',
+      year: 'Senior',
+      hometown: 'Santa Barbara, CA',
+      major: 'Political Science',
+    },
+    {
+      name: 'Chris Chen',
+      side: 'Port',
+      year: 'Junior',
+      hometown: 'Arcadia, CA',
+      major: 'Chemical Biology',
+    },
+    {
+      name: 'Daniel Chang',
+      side: 'Starboard',
+      year: 'Sophomore',
+      hometown: 'Hightstown, NJ',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'David Yang',
+      side: 'Port',
+      year: 'Junir',
+      hometown: 'Toronto, ON',
+      major: 'Chemical & Material Science Engineering',
+    },
+    {
+      name: 'Edison Wang',
+      side: 'Coxswain',
+      year: 'Sophomore',
+      hometown: 'Canton, China',
+      major: 'Cognitive Science',
+    },
+    {
+      name: 'George Man',
+      side: 'Starboard',
+      year: 'Sophomore',
+      hometown: 'San Francisco, CA',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Ishaan Golding',
+      side: 'Coxswain',
+      year: 'Grad Student',
+      hometown: 'Austin, Texas',
+      major: 'Political Science',
+    },
+    {
+      name: 'Jake Tennant',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'Sacramento, CA',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Jason Ke',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'San Diego, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Jose Fernandez',
+      side: 'Port',
+      year: 'Sophomore',
+      hometown: 'Fullerton, CA',
+      major: 'Ethnic Studies',
+    },
+    {
+      name: 'Michael Giron',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Hemet, CA',
+      major: 'Development Studies',
+    },
+    {
+      name: 'Palmer Hayward',
+      side: 'No Preference',
+      year: 'Senior',
+      hometown: 'San Diego, CA',
+      major: 'Mechanical Engineering',
+    },
+    {
+      name: 'Thomas Dwelley',
+      side: 'Starboard',
+      year: 'Grad Student',
+      hometown: 'Santa Barbara, CA',
+      major: 'Mechanical Engineering',
+    },
+  ];
+
+  const novicePlayers = [
+    {
+      name: 'Andrew Capistrano',
+      side: 'No Preference',
+      year: 'Sophomore',
+      hometown: 'Hong Kong, China',
+      major: 'Environmental Science',
+    },
+    {
+      name: 'Andrew Chi',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Garden Grove, CA',
+      major: 'Economics & Pre-Business',
+    },
+    {
+      name: 'Antonio Sakkis',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Houston, TX',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Chris Webster',
+      side: 'Starboard',
+      year: 'Sophomore',
+      hometown: 'Stockton, CA',
+      major: 'Economics',
+    },
+    {
+      name: 'Chan Varma',
+      side: 'Coxswain',
+      year: 'Freshman',
+      hometown: 'Bangalore, India',
+      major: 'Computer Science',
+    },
+    {
+      name: 'Eric Vengrin',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'San Diego, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Derek Feng',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Fremont, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Jake Yukich',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Los Angeles, CA',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Jameson Mah',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Oxnard, CA',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Jeffrey Liu',
+      side: 'Port',
+      year: 'Junior',
+      hometown: 'Palo Alto, CA',
+      major: 'Electrical Engineering & Comp. Sci',
+    },
+    {
+      name: 'Jonathan Lin',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'San Jose, CA',
+      major: 'Chemical Biology & Music',
+    },
+    {
+      name: 'Jonathan Lowery',
+      side: 'Port',
+      year: 'Junior',
+      hometown: 'San Diego, CA',
+      major: 'Economics',
+    },
+    {
+      name: 'Joshua Sanders',
+      side: 'Port',
+      year: 'Junior',
+      hometown: 'Clearwater, FL',
+      major: 'Music',
+    },
+    {
+      name: 'Kevin Mahoney',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'East Richmond Heights, CA',
+      major: 'Civil & Environmental Engineering',
+    },
+    {
+      name: 'Luca Amato',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Los Angeles, CA',
+      major: 'Political Science',
+    },
+    {
+      name: 'Magne Ledsaak',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Stavanger, Norway',
+      major: 'Industrial Economics',
+    },
+    {
+      name: 'Mateo Lopez',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Bakersfield, CA',
+      major: 'Public Health',
+    },
+    {
+      name: 'Michael Eliot',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'Huntington Beach, CA',
+      major: 'Pre-Business and Computer Science',
+    },
+    {
+      name: 'Omid Rhezaii',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'San Diego, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Peter Birghoffer',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Budapest, Hungary',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Salim Dharamshi',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Nairobi, Kenya',
+      major: 'Nairobi, Kenya',
+    },
+    {
+      name: 'Samson Mataraso',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Walnut Creek, CA',
+      major: 'Bioengineering',
+    },
+    {
+      name: 'Shaun Singh',
+      side: 'Port',
+      year: 'Senior',
+      hometown: 'Berkeley, CA',
+      major: 'Economics',
+    },
+    {
+      name: 'Shahzad Shaukat',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Lahore, Pakistan',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Taeho Jung',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Busan, South Korea',
+      major: 'Japanese',
+    },
+    {
+      name: 'Taylor Wong',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Cary, NC',
+      major: 'Electrical Engineering & Comp. Sci.',
+    },
+    {
+      name: 'Tristan Wasley',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Laguna Hills, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Windsor Taro',
+      side: 'No Preference',
+      year: 'Senior',
+      hometown: 'San Antonio, TX',
+      major: 'Economics',
+    },
+  ];
+
+  return (
+    <div className="container">
+      <div className="page__header">
+        2015-2016 Varsity & Novice Men
+      </div>
+      <div className="image">
+        <img alt="" src={photo} />
+      </div>
+      <div className="roster">
+        <Roster
+          rosterName={'Varsity Men'}
+          names={varsityPlayers.map((person) => (
+            person.name
+          ))}
+          sides={varsityPlayers.map((person) => (
+            person.side
+          ))}
+          years={varsityPlayers.map((person) => (
+            person.year
+          ))}
+          hometowns={varsityPlayers.map((person) => (
+            person.hometown
+          ))}
+          majors={varsityPlayers.map((person) => (
+            person.major
+          ))}
+        />
+      </div>
+      <div className="roster">
+        <Roster
+          rosterName={'Novice Men'}
+          names={novicePlayers.map((person) => (
+            person.name
+          ))}
+          sides={novicePlayers.map((person) => (
+            person.side
+          ))}
+          years={novicePlayers.map((person) => (
+            person.year
+          ))}
+          hometowns={novicePlayers.map((person) => (
+            person.hometown
+          ))}
+          majors={novicePlayers.map((person) => (
+            person.major
+          ))}
+        />
+      </div>
     </div>
-    <div className="image">
-      <img alt="" src={photo} />
-    </div>
-    <div className="roster">
-      <Roster
-        rosterName={'Varsity Men'}
-        names={[
-          'Amos Frank',
-          'Andy Chung',
-          'Arthur Sebes',
-          'Ben Bavonese',
-          'Brian Osgood',
-          'Chris Chen',
-          'Daniel Chang',
-          'David Yang',
-          'Edison Wong',
-          'George Man',
-          'Ishaan Golding',
-          'Jake Tennant',
-          'Jason Ke',
-          'Jose Fernandez',
-          'Michael Giron',
-          'Palmer Hayward',
-          'Thomas Dwelley',
-        ]}
-        sides={[
-          'no preference',
-          'starboard',
-          'no preference',
-          'port',
-          'port',
-          'port',
-          'starboard',
-          'port',
-          'coxswain',
-          'starboard',
-          'coxswain',
-          'no preference',
-          'starboard',
-          'port',
-          'starboard',
-          'no preference',
-          'starboard',
-        ]}
-        years={[
-          'Freshman',
-          'Junior',
-          'Grad Student',
-          'Freshman',
-          'Senior',
-          'Junior',
-          'Sophomore',
-          'Junior',
-          'Sophomore',
-          'Sophomore',
-          'Sophomore',
-          'Freshman',
-          'Freshman',
-          'Sophomore',
-          'Junior',
-          'Senior',
-          'Junior',
-        ]}
-        hometowns={[
-          'Vancouver, BC',
-          'Burbank, CA',
-          'Lyon, France',
-          'Ann Arbor, MI',
-          'Santa Barbara, CA',
-          'Arcadia, CA',
-          'Hightstown, NJ',
-          'Toronto, ON',
-          'Canton, China',
-          'San Francisco, CA',
-          'Austin, TX',
-          'Sacramento, CA',
-          'San Diego, CA',
-          'Fullerton, CA',
-          'Hemet, CA',
-          'San Diego, CA',
-          'Santa Barbara, CA',
-        ]}
-        majors={[
-          'Undeclared',
-          'Economics',
-          'Transportation Engineering',
-          'Economics',
-          'Political Science',
-          'Chemical Biology',
-          'Molecular & Cell Biology',
-          'Chemical & Material Science Eng.',
-          'Cognitive Science',
-          'Molecular & Cell Biology',
-          'Political Science',
-          'Molecular & Cell Biology',
-          'Undeclared',
-          'Ethnic Studies',
-          'Development Studies',
-          'Mechanical Engineering',
-          'Mechanical Engineering',
-        ]}
-      />
-    </div>
-    <div className="roster">
-      <Roster
-        rosterName={'Novice Men'}
-        names={[
-          'Andrew Capistrano',
-          'Andrew Chi',
-          'Antonio Sakkis',
-          'Chris Webster',
-          'Chan Varma',
-          'Eric Vengrin',
-          'Derek Feng',
-          'Jake Yukich',
-          'Jameson Mah',
-          'Jeffrey Liu',
-          'Jonathan Lin',
-          'Jonathan Lowery',
-          'Joshua Sanders',
-          'Kevin Mahoney',
-          'Luca Amato',
-          'Magne Ledsaak',
-          'Mateo Lopez',
-          'Michael Eliot',
-          'Omid Rhezaii',
-          'Peter Birghoffer',
-          'Salim Dharamshi',
-          'Samson Mataraso',
-          'Shaun Singh',
-          'Shahzad Shaukat',
-          'Taeho Jung',
-          'Taylor Wong',
-          'Tristan Wasley',
-          'Windsor Taro',
-        ]}
-        sides={[
-          'no preference',
-          'starboard',
-          'port',
-          'starboard',
-          'coxswain',
-          'no preference',
-          'port',
-          'port',
-          'starboard',
-          'port',
-          'starboard',
-          'port',
-          'port',
-          'no preference',
-          'starboard',
-          'starboard',
-          'port',
-          'no preference',
-          'starboard',
-          'starboard',
-          'starboard',
-          'port',
-          'port',
-          'starboard',
-          'starboard',
-          'starboard',
-          'starboard',
-          'no preference',
-        ]}
-        years={[
-          'Sophomore',
-          'Freshman',
-          'Freshman',
-          'Sophomore',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Junior',
-          'Junior',
-          'Junior',
-          'Senior',
-          'Freshman',
-          'Freshman',
-          'Junior',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Senior',
-          'Freshman',
-          'Junior',
-          'Freshman',
-          'Freshman',
-          'Senior',
-        ]}
-        hometowns={[
-          'Hong Kong, China',
-          'Garden Grove, CA',
-          'Houston, TX',
-          'Stockton, CA',
-          'Bangalore, India',
-          'San Diego, CA',
-          'Fremont, CA',
-          'Los Angeles, CA',
-          'Oxnard, CA',
-          'Palo Alto, CA',
-          'San Jose, CA',
-          'San Diego, CA',
-          'Clearwater, FL',
-          'East Richmond Heights, CA',
-          'Los Angeles, CA',
-          'Stavanger, Norway',
-          'Bakersfield, CA',
-          'Huntington Beach, CA',
-          'San Diego, CA',
-          'Budapest, Hungary',
-          'Nairobi, Kenya',
-          'Walnut Creek, CA',
-          'Berkeley, CA',
-          'Lahore, Pakistan',
-          'Busan, South Korea',
-          'Cary, NC',
-          'Laguna Hills, CA',
-          'San Antonio, TX',
-        ]}
-        majors={[
-          'Environmental Science',
-          'Economics & Pre-Business',
-          'Undeclared',
-          'Economics',
-          'Computer Science',
-          'Undeclared',
-          'Undeclared',
-          'Molecular & Cell Biology',
-          'Molecular & Cell Biology',
-          'Electrical Engineering & Comp. Sci.',
-          'Chemical Biology & Music',
-          'Economics',
-          'Music',
-          'Civil & Environmental Engineering',
-          'Political Science',
-          'Industrial Economics',
-          'Public Health',
-          'Pre-Business and Computer Science',
-          'Undeclared',
-          'Undeclared',
-          'History & Economics',
-          'Bioengineering',
-          'Economics',
-          'Undeclared',
-          'Japanese',
-          'Electrical Engineering & Comp. Sci.',
-          'Undeclared',
-          'Economics',
-        ]}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default RosterPage;
--- pages/roster/women.js
+++ pages/roster/women.js
@@ -2,157 +2,219 @@ import React from 'react';
 import Roster from '../../components/Roster';
 import photo from '../../assets/images/womensroster.jpg';
 
-import '../../css/rosters.scss';
+import '../../css/Roster.scss';
 
-const RosterPage = () => (
-  <div className="container">
-    <div className="page__header">
-      2015-2016 Varsity & Novice Women
+const RosterPage = () => {
+  const varsityPlayers = [
+    {
+      name: 'Cassie Redding',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Pasadena, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Colby Gekko',
+      side: 'Port',
+      year: 'Senior',
+      hometown: 'San Clemente, CA',
+      major: 'Chemistry & Molecular & Cell Bio.',
+    },
+    {
+      name: 'Hannah Rudoff',
+      side: 'Port',
+      year: 'Senior',
+      hometown: 'Portland, OR',
+      major: 'Political Science & Economics',
+    },
+    {
+      name: 'Lydia Chen',
+      side: 'No Preference',
+      year: 'Junior',
+      hometown: 'Burke, VA',
+      major: 'Dietetics',
+    },
+    {
+      name: 'Monisha Lensink-Vasan',
+      side: 'No Preference',
+      year: 'Junior',
+      hometown: 'Denver, CO',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Philine Endres',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Riverside, CA',
+      major: 'Interdisciplinary Studies',
+    },
+    {
+      name: 'Reni Forer',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Westport, CT',
+      major: 'Cognitive Science',
+    },
+    {
+      name: 'Samantha Lai',
+      side: 'Port',
+      year: 'Junior',
+      hometown: 'Piedmont, CA',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Tori Cabot',
+      side: 'Port',
+      year: 'Senior',
+      hometown: 'Holliston, MA',
+      major: 'Media Studies',
+    },
+  ];
+
+  const novicePlayers = [
+    {
+      name: 'Amanda Chung',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Santa Barbara, CA',
+      major: 'Political Science',
+    },
+    {
+      name: 'Annie Speckhahn',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'Reno, NV',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Aparna Dhinakaran',
+      side: 'Port',
+      year: 'Senior',
+      hometown: 'Fremont, CA',
+      major: 'Electrical Engineering & Comp. Sci.',
+    },
+    {
+      name: 'Brittany Nguyen',
+      side: 'Coxswain',
+      year: 'Freshman',
+      hometown: 'San Jose, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Janice Luong',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'Rowland Heights, CA',
+      major: 'Integrative Biology',
+    },
+    {
+      name: 'Jeannie O&#39;Driscoll',
+      side: 'No Preference',
+      year: 'Junior',
+      hometown: 'Cord, Ireland',
+      major: 'Engineering (Exchange)',
+    },
+    {
+      name: 'Kim Sakai',
+      side: 'Coxswain',
+      year: 'Junior',
+      hometown: 'Morgan Hill, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Kylie Sammons',
+      side: 'Starboard',
+      year: 'Junior',
+      hometown: 'Corona, CA',
+      major: 'Anthropology',
+    },
+    {
+      name: 'Liz Go',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: 'La Quinta, CA',
+      major: 'Molecular & Cell Biology',
+    },
+    {
+      name: 'Maddie Huber',
+      side: 'Port',
+      year: 'Freshman',
+      hometown: '',
+      major: 'Nutrition',
+    },
+    {
+      name: 'Madie Dunlap',
+      side: 'Starboard',
+      year: 'Freshman',
+      hometown: 'Chico, CA',
+      major: 'Mathematics',
+    },
+    {
+      name: 'Minna Brown',
+      side: 'No Preference',
+      year: 'Freshman',
+      hometown: 'Oakland, CA',
+      major: 'Undeclared',
+    },
+    {
+      name: 'Zoe Ziegler',
+      side: 'Coxswain',
+      year: 'Freshman',
+      hometown: 'Arcata, CA',
+      major: 'Undeclared',
+    },
+  ];
+
+  return (
+    <div className="container">
+      <div className="page__header">
+        2015-2016 Varsity & Novice Men
+      </div>
+      <div className="image">
+        <img alt="" src={photo} />
+      </div>
+      <div className="roster">
+        <Roster
+          rosterName={'Varsity Women'}
+          names={varsityPlayers.map((person) => (
+            person.name
+          ))}
+          sides={varsityPlayers.map((person) => (
+            person.side
+          ))}
+          years={varsityPlayers.map((person) => (
+            person.year
+          ))}
+          hometowns={varsityPlayers.map((person) => (
+            person.hometown
+          ))}
+          majors={varsityPlayers.map((person) => (
+            person.major
+          ))}
+        />
+      </div>
+      <div className="roster">
+        <Roster
+          rosterName={'Novice Women'}
+          names={novicePlayers.map((person) => (
+            person.name
+          ))}
+          sides={novicePlayers.map((person) => (
+            person.side
+          ))}
+          years={novicePlayers.map((person) => (
+            person.year
+          ))}
+          hometowns={novicePlayers.map((person) => (
+            person.hometown
+          ))}
+          majors={novicePlayers.map((person) => (
+            person.major
+          ))}
+        />
+      </div>
     </div>
-    <div className="image">
-      <img alt="" src={photo} />
-    </div>
-    <div className="roster">
-      <Roster
-        rosterName={'Varsity Women'}
-        names={[
-          'Cassie Redding',
-          'Colby Gekko',
-          'Hannah Rudoff',
-          'Lydia Chen',
-          'Monisha Lensink-Vasan',
-          'Philine Endres',
-          'Reni Forer',
-          'Samantha Lai',
-          'Tori Cabot',
-        ]}
-        sides={[
-          'Starboard',
-          'Port',
-          'Port',
-          'No preference',
-          'No preference',
-          'Starboard',
-          'Starboard',
-          'Port',
-          'Port',
-        ]}
-        years={[
-          'Junior',
-          'Senior',
-          'Senior',
-          'Junior',
-          'Junior',
-          'Junior',
-          'Junior',
-          'Junior',
-          'Senior',
-        ]}
-        hometowns={[
-          'Pasadena, CA',
-          'San Clemente, CA',
-          'Portland, OR',
-          'Burke, VA',
-          'Denver, CO',
-          'Riverside, CA',
-          'Westport, CT',
-          'Piedmont, CA',
-          'Holliston, MA',
-        ]}
-        majors={[
-          'Undeclared',
-          'Chemistry & Molecular & Cell Bio.',
-          'Political Science & Economics',
-          'Dietetics',
-          'Molecular & Cell Biology',
-          'Interdisciplinary Studies',
-          'Cognitive Science',
-          'Molecular & Cell Biology',
-          'Media Studies',
-        ]}
-      />
-    </div>
-    <div className="roster">
-      <Roster
-        rosterName={'Novice Women'}
-        names={[
-          'Amanda Chung',
-          'Annie Speckhahn',
-          'Aparna Dhinakaran',
-          'Brittany Nguyen',
-          'Janice Luong',
-          'Jeannie O\'Driscoll',
-          'Kim Sakai',
-          'Kylie Sammons',
-          'Liz Go',
-          'Maddie Huber',
-          'Madie Dunlap',
-          'Minna Brown',
-          'Zoe Ziegler',
-        ]}
-        sides={[
-          'Port',
-          'No Preference',
-          'Port',
-          'Coxswain',
-          'Port',
-          'No preference',
-          'Coxswain',
-          'Starboard',
-          'Port',
-          'Port',
-          'Starboard',
-          'No preference',
-          'Coxswain',
-        ]}
-        years={[
-          'Freshman',
-          'Freshman',
-          'Senior',
-          'Freshman',
-          'Freshman',
-          'Junior',
-          'Junior',
-          'Junior',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-          'Freshman',
-        ]}
-        hometowns={[
-          'Santa Barbara, CA',
-          'Reno, NV',
-          'Fremont, CA',
-          'San Jose, CA',
-          'Rowland Heights, CA',
-          'Cork, Ireland',
-          'Morgan Hill, CA',
-          'Corona, CA',
-          'La Quinta, CA',
-          'Napa, CA',
-          'Chico, CA',
-          'Oakland, CA',
-          'Arcata, CA',
-        ]}
-        majors={[
-          'Political Science',
-          'Undeclared',
-          'Electrical Engineering & Comp. Sci.',
-          'Undeclared',
-          'Integrative Biology',
-          'Engineering (Exchange)',
-          'Undeclared',
-          'Anthropology',
-          'Molecular & Cell Biology',
-          'Nutrition',
-          'Mathematics',
-          'Undeclared',
-          'Undeclared',
-        ]}
-      />
-    </div>
-  </div>
-);
+  );
+};
 
 export default RosterPage;
