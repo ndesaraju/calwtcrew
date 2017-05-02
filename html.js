2ec7629920d76befb7b21b745780fc6529b3d907
Alex Pan <alpan@berkeley.edu>
2017-05-02 13:30:38 -0700

    favicon  (#18)
    * fixed icon + join
    
    * fixed hamburger, made it close when a link is clicked
    
    * favicon
    
    * changed favicon location

--- html.js
+++ html.js
@@ -18,6 +18,8 @@ const Html = ({ body }) => {
 
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
         <link href="https://fonts.googleapis.com/css?family=Montserrat:300,400,500,600,900" rel="stylesheet" />
+  
+        <link rel="icon" type="img/png" href={ prefixLink('/favicon.png') } />
 
         {head.title.toComponent()}
         {head.meta.toComponent()}
