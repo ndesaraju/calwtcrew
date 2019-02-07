ccaa81ef8794dac2340615ed65148ebf514fab06
Niharika Desaraju <niharika.desaraju@berkeley.edu>
2019-02-06 16:20:45 -0800

    in progress

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,9 +1,3 @@
-exports.modifyWebpackConfig = (config, stage) => {
-  config.loader('eslint', {
-    test: /\.js$/,
-    exclude: /node_modules/,
-  })
-
-  return config;
+exports.onCreateWebpackConfig = (stage, actions) => {
 };
 
